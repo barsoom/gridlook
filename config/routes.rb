c9707f7eb35026ed853ffddaf71730d2b6cdb34d
Henrik N <henrik@nyh.se>
2013-03-17 18:06:26 +0100

    Welcome page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Gridlook::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+  root to: "welcome#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
