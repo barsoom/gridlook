17bb78981f7b7ca37140b8d8d106e7147848126a
Henrik N <henrik@nyh.se>
2013-03-17 19:44:10 +0100

    Get secret_token from Heroku ENV.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,7 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Gridlook::Application.config.secret_key_base = '9cd6c5830609bb6254377d65666dd77df8c55a4e37a6fc2ccc1514c085c747fbc14e48aeef8a75c26d6320da0cfe04233ed0375f15684da4957a0527f53991fe'
+
+# The default key is just for dev.
+# Production gets another key from an ENV.
+Gridlook::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || '9cd6c5830609bb6254377d65666dd77df8c55a4e37a6fc2ccc1514c085c747fbc14e48aeef8a75c26d6320da0cfe04233ed0375f15684da4957a0527f53991fe'
