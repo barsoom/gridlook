9516b11a374a4fd9ef0cdf143c985fe5ab248730
Henrik N <henrik@nyh.se>
2013-03-19 09:33:39 +0100

    Basic 'lists events' spec.

--- spec/features/browsing_spec.rb
+++ spec/features/browsing_spec.rb
@@ -1,8 +1,22 @@
 require "spec_helper"
 
 describe "Browsing" do
-  it "works" do
+  it "lists events" do
+    create_event
     visit root_path
     page.should have_content "Gridlook"
+    page.should have_content "foo@example.com"
+    page.should have_content "sent"
+  end
+
+  def create_event
+    Event.create!(
+      email: "foo@example.com",
+      name: "sent",
+      happened_at: Time.now,
+      arguments: { hello: "there" },
+      category: [ "one", "two" ],
+      data: { url: "http://example.com/foo" }
+    )
   end
 end
