18f689b5a57843deb69a704a722258998d376428
Henrik N <henrik@nyh.se>
2013-03-19 11:05:56 +0100

    Prettier.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def pretty_inspect(x)
+    require "pp"
+    x.pretty_inspect
+  end
+
+  def data_if_present(data, title)
+    if data.present?
+      content_tag(:h4, title) + pretty_inspect(data)
+    end
+  end
 end
