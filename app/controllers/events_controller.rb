081588fac924069083464fb61204530c5cf49841
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2013-03-19 09:21:35 +0100

    Add json support.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,11 +4,17 @@ class EventsController < ApplicationController
   def index
     email = params[:email].to_s.strip.presence
     page = params[:page]
+    events = Event.email(email).recent(page, PER_PAGE)
 
-    render locals: {
-      count:  Event.count,
-      email: email,
-      events: Event.email(email).recent(page, PER_PAGE)
-    }
+    respond_to do |format|
+      format.html {
+        render locals: {
+          count:  Event.count,
+          email: email,
+          events: events
+        }
+      }
+      format.json { render json: events.to_json }
+    end
   end
 end
