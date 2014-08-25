07311679aca2c8e1c95453d872d8bc5bc422a0ba
Tomas Skogberg <tomas.skogberg@gmail.com>
2014-08-25 10:23:51 +0200

    Fix missing syntax
    Forgot semicolons and used the wrong table name on the drop trigger
    command.

--- db/migrate/20140825062349_drop_trigger_and_function.rb
+++ db/migrate/20140825062349_drop_trigger_and_function.rb
@@ -3,9 +3,9 @@ class DropTriggerAndFunction < ActiveRecord::Migration
     if Rails.env.production?
       transaction do
         execute <<-SQL
-        DROP TRIGGER IF EXISTS countrows ON rowcount
-        DROP FUNCTION IF EXISTS count_rows()
-        DROP TABLE IF EXISTS rowcount
+        DROP TRIGGER IF EXISTS countrows ON events;
+        DROP FUNCTION IF EXISTS count_rows();
+        DROP TABLE IF EXISTS rowcount;
         SQL
       end
     end
