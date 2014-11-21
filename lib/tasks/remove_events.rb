09f191ccc60d13d9bd1e269a65256465698885ab
Tomas Skogberg <tomas.skogberg@gmail.com>
2014-11-21 13:26:24 +0100

    Remove events: Forgot to update total events
    Since we use a separate table to count the total number of events,
     we need to update it as well.

--- lib/tasks/remove_events.rb
+++ lib/tasks/remove_events.rb
@@ -9,7 +9,14 @@ class RemoveEvents
 
   def destroy_events_older_than_six_months
     time = 6.months.ago
-    Event.where("created_at < ?", time).destroy_all
+    # We have no associated relations so we can use delete_all
+    number_of_events_deleted = Event.where("created_at < ?", time).delete_all
+    update_total_events(number_of_events_deleted)
     puts "Deleted events older than #{time}"
+  end
+
+  def update_total_events(number_of_events_deleted)
+    old_total_events = EventsData.total_events
+    EventsData.instance.decrement!(:total_events, old_total_events - number_of_events_deleted)
   end
 end
--- spec/lib/tasks/remove_events_spec.rb
+++ spec/lib/tasks/remove_events_spec.rb
@@ -2,17 +2,19 @@ require "rails_helper"
 require "tasks/remove_events"
 
 describe RemoveEvents, ".run" do
-  it "removes data older than six months" do
+  it "removes data older than six months and updates total events" do
     # No need for the logging output in the test
     allow_any_instance_of(RemoveEvents).to receive(:puts)
 
     event = create_event_at(6.months.ago + 1.second)
     event = create_event_at(6.months.ago - 1.second)
     expect(Event.count).to eq(2)  # Sanity
+    expect(EventsData.total_events).to eq(2)  # Sanity
 
     RemoveEvents.run
 
     expect(Event.count).to eq(1)
+    expect(EventsData.total_events).to eq(1)
   end
 
   private
