4cc218b81033a567a1b07ea9571ae69ea00fd3b9
Tomas Skogberg <tomas.skogberg@gmail.com>
2014-11-21 13:49:37 +0100

    RemoveEvents: Fix total events

--- lib/tasks/remove_events.rb
+++ lib/tasks/remove_events.rb
@@ -11,12 +11,7 @@ class RemoveEvents
     time = 6.months.ago
     # We have no associated relations so we can use delete_all
     number_of_events_deleted = Event.where("created_at < ?", time).delete_all
-    update_total_events(number_of_events_deleted)
+    EventsData.instance.decrement!(:total_events, number_of_events_deleted)
     puts "Deleted events older than #{time}"
-  end
-
-  def update_total_events(number_of_events_deleted)
-    old_total_events = EventsData.total_events
-    EventsData.instance.decrement!(:total_events, old_total_events - number_of_events_deleted)
   end
 end
--- spec/lib/tasks/remove_events_spec.rb
+++ spec/lib/tasks/remove_events_spec.rb
@@ -6,8 +6,10 @@ describe RemoveEvents, ".run" do
     # No need for the logging output in the test
     allow_any_instance_of(RemoveEvents).to receive(:puts)
 
-    event = create_event_at(6.months.ago + 1.second)
-    event = create_event_at(6.months.ago - 1.second)
+    older_than_six_month = 6.months.ago - 1.second
+    newer_than_six_month = 6.months.ago + 1.second
+    create_event_at(older_than_six_month)
+    create_event_at(newer_than_six_month)
     expect(Event.count).to eq(2)  # Sanity
     expect(EventsData.total_events).to eq(2)  # Sanity
 
