26160af0855403c9fa3adc9299c00bc47aa55fb2
Tomas Skogberg <tomas.skogberg@gmail.com>
2014-11-27 09:42:19 +0100

    Cleanup

--- spec/lib/tasks/remove_events_spec.rb
+++ spec/lib/tasks/remove_events_spec.rb
@@ -6,15 +6,15 @@ describe RemoveEvents, ".run" do
     # No need for the logging output in the test
     allow_any_instance_of(RemoveEvents).to receive(:puts)
 
-    event_older_than_six_month = create_event_at(6.months.ago - 1.second)
-    event_newer_than_six_month = create_event_at(6.months.ago + 1.second)
+    event_older_than_six_months = create_event_at(6.months.ago - 1.second)
+    event_newer_than_six_months = create_event_at(6.months.ago + 1.second)
     expect(Event.count).to eq(2)  # Sanity
     expect(EventsData.total_events).to eq(2)  # Sanity
 
     RemoveEvents.run
 
-    expect { event_older_than_six_month.reload }.to raise_error(ActiveRecord::RecordNotFound)
-    expect(Event.find(event_newer_than_six_month).id).to eq(event_newer_than_six_month.id)
+    expect { event_older_than_six_months.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    expect(Event.find(event_newer_than_six_months).id).to eq(event_newer_than_six_months.id)
     expect(Event.count).to eq(1)
     expect(EventsData.total_events).to eq(1)
   end
