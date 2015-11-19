70808f5c66ebb5341a601917cc2d048356ca7ceb
Tomas Skogberg <tomas.skogberg@gmail.com>
2015-11-19 09:04:18 +0100

    Fix broken test

--- spec/lib/tasks/remove_events_spec.rb
+++ spec/lib/tasks/remove_events_spec.rb
@@ -2,19 +2,19 @@ require "rails_helper"
 require "tasks/remove_events"
 
 describe RemoveEvents, ".call" do
-  it "removes data older than six months and updates total events" do
+  it "removes data older than five months and updates total events" do
     # No need for the logging output in the test
     allow_any_instance_of(RemoveEvents).to receive(:puts)
 
-    event_older_than_six_months = create_event_at(6.months.ago - 1.second)
-    event_newer_than_six_months = create_event_at(6.months.ago + 1.second)
+    event_older_than_five_months = create_event_at(5.months.ago - 1.second)
+    event_newer_than_five_months = create_event_at(5.months.ago + 1.second)
     expect(Event.count).to eq(2)  # Sanity
     expect(EventsData.total_events).to eq(2)  # Sanity
 
     RemoveEvents.call
 
-    expect { event_older_than_six_months.reload }.to raise_error(ActiveRecord::RecordNotFound)
-    expect { event_newer_than_six_months.reload }.not_to raise_error
+    expect { event_older_than_five_months.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    expect { event_newer_than_five_months.reload }.not_to raise_error
     expect(EventsData.total_events).to eq(1)
   end
 
