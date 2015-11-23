c3c6140001556ed8d6bb3c3a9590b9eb220abcb2
Tomas Skogberg <tomas.skogberg@gmail.com>
2015-11-23 09:25:31 +0100

    Cleanup

--- spec/lib/tasks/remove_events_spec.rb
+++ spec/lib/tasks/remove_events_spec.rb
@@ -2,19 +2,20 @@ require "rails_helper"
 require "tasks/remove_events"
 
 describe RemoveEvents, ".call" do
-  it "removes data older than five months and updates total events" do
+  it "removes data older than the limit and updates total events" do
     # No need for the logging output in the test
+    limit = 5.months.ago
     allow_any_instance_of(RemoveEvents).to receive(:puts)
 
-    event_older_than_five_months = create_event_at(5.months.ago - 1.second)
-    event_newer_than_five_months = create_event_at(5.months.ago + 1.second)
+    event_older_than_the_limit = create_event_at(limit - 1.second)
+    event_newer_than_the_limit = create_event_at(limit + 1.second)
     expect(Event.count).to eq(2)  # Sanity
     expect(EventsData.total_events).to eq(2)  # Sanity
 
     RemoveEvents.call
 
-    expect { event_older_than_five_months.reload }.to raise_error(ActiveRecord::RecordNotFound)
-    expect { event_newer_than_five_months.reload }.not_to raise_error
+    expect { event_older_than_the_limit.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    expect { event_newer_than_the_limit.reload }.not_to raise_error
     expect(EventsData.total_events).to eq(1)
   end
 
