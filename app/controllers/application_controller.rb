a4150c2aaadd62b2f1e44ad26e0140a15315aaa6
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-03-18 16:58:25 +0100

    Only auth in production, but require it there
    Fixes tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD']
+  if Rails.env.production?
+    http_basic_authenticate_with name: ENV.fetch("HTTP_USER"), password: ENV.fetch("HTTP_PASSWORD")
+  end
 end
