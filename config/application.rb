2413d4b7a6cf8d458c72d958714141943119b1ff
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-09-02 21:30:11 +0200

    Enable revision check without auth

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module Gridlook
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.middleware.insert_after ActionDispatch::Session::CookieStore, JwtAuthentication
+    config.middleware.insert_after ActionDispatch::Session::CookieStore, JwtAuthentication, except: /^\/revision$/
   end
 end
