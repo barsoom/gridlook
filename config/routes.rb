e91e9ae212d847fbb798969bef74d2995c5a05dd
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-09-03 09:23:32 +0200

    Clarify

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Gridlook::Application.routes.draw do
   # Used for checks by deploy scripts
   get "revision" => ->(_) { [ 200, {}, [ ENV.fetch("GIT_COMMIT") ] ] }
 
+  # post "/events" is added by Gridhook
+
+  # NOTE: If you change anything here, also check JwtAuthentication config in application.rb
+
   root to: 'events#index'
 end
