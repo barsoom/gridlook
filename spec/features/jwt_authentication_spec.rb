4f700510a577412fe5a238d32f15545d48828d26
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-09-15 16:54:17 +0200

    Update jwt_authentication

--- spec/features/jwt_authentication_spec.rb
+++ spec/features/jwt_authentication_spec.rb
@@ -7,7 +7,6 @@ describe "JWT authenticate" do
   before do
     ENV["JWT_SESSION_TIMEOUT_IN_SECONDS"] = "600"
     ENV["JWT_KEY"] = secret_key
-    ENV["JWT_PARAM_NAME"] = "token"
     ENV["JWT_PARAM_MISSING_REDIRECT_URL"] = "http://example.com/request_jwt_auth?app=gridlook"
     ENV["JWT_ALGORITHM"] = "HS512"
   end
@@ -19,7 +18,7 @@ describe "JWT authenticate" do
   it "can authenticate with JWT" do
     # Shows you gridlook at the correct URL when authenticated
     token = build_token(secret: secret_key)
-    visit "/?token=#{token}"
+    visit "/?jwt_authentication_token=#{token}"
     expect(page.status_code).to eq(200)
     expect(page).to have_content("Gridlook")
     expect(current_url).to eq("http://www.example.com/")
@@ -27,14 +26,14 @@ describe "JWT authenticate" do
 
   it "does not provide access without a valid token" do
     token = build_token(secret: invalid_secret_key)
-    visit "/?token=#{token}"
+    visit "/?jwt_authentication_token=#{token}"
     expect(page.status_code).to eq(403)
   end
 
   it "does not authenticate with JWT when there it is not configured" do
     ENV["JWT_KEY"] = nil
     token = build_token(secret: invalid_secret_key)
-    visit "/?token=#{token}"
+    visit "/?jwt_authentication_token=#{token}"
     expect(page.status_code).to eq(200)
     expect(page).to have_content("Gridlook")
   end
