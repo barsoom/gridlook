a07267a876b1d29b417725029301bf6ee9b481ce
Tomas Skogberg <tomas.skogberg@gmail.com>
2016-12-02 11:10:32 +0100

    Fix quote style

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] ||= "test"
 
 require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'capybara/rails'
-require 'capybara/rspec'
+require "rspec/rails"
+require "capybara/rails"
+require "capybara/rspec"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
