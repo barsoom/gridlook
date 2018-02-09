5cf7442e3d53883024550a07c4eb02ea2b749641
Tomas Skogberg <tomas.skogberg@gmail.com>
2018-02-09 14:05:36 +0100

    Add missing basic auth test for /events end-point

--- spec/requests/creating_events_spec.rb
+++ spec/requests/creating_events_spec.rb
@@ -1,9 +1,21 @@
 require "rails_helper"
 
 describe "The webhook" do
-  it "creates events" do
+  include Rack::Test::Methods
+
+  it "creates events with basic auth enabled" do
+    allow(ENV).to receive(:[]) do |key|
+      case key
+      when "HTTP_USER"
+        "foobar"
+      when "HTTP_PASSWORD"
+        "secret"
+      end
+    end
+
     expect {
-      post "/events", params: { email: "foo@example.com" }
+      basic_authorize "foobar", "secret"
+      post "/events", { email: "foo@example.com" }.to_json
     }.to change(Event, :count).by(1)
 
     event = Event.last
