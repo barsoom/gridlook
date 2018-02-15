545ae55db1a8888346686147a58596abd246190e
Tomas Skogberg <tomas.skogberg@gmail.com>
2018-02-15 13:48:19 +0100

    Add failing basic auth test

--- spec/requests/creating_events_spec.rb
+++ spec/requests/creating_events_spec.rb
@@ -21,4 +21,21 @@ describe "The webhook" do
     event = Event.last
     expect(event.email).to eq("foo@example.com")
   end
+
+  it "does not create events when basic auth fails" do
+    allow(ENV).to receive(:[]) do |key|
+      case key
+      when "HTTP_USER"
+        nil
+      when "HTTP_PASSWORD"
+        nil
+      end
+    end
+
+    basic_authorize "foobar", "secret"
+    post "/events", { email: "foo@example.com" }.to_json
+
+    expect(Event.count).to eq(0)
+  end
+
 end
