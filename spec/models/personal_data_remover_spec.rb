d630c036fd99d501abe1b1a1fb44ec83db666979
Henrik Nyh <henrik@nyh.se>
2018-06-26 16:23:46 +0200

    PersonalDataRemover: Spec that it doesn't remove other events
    Pretty important to be confident in this…

--- spec/models/personal_data_remover_spec.rb
+++ spec/models/personal_data_remover_spec.rb
@@ -3,14 +3,15 @@ require "rails_helper"
 describe PersonalDataRemover, ".call" do
   it "removes events for that email, case-insensitive" do
     event = Event.create!(email: "FoO@ExAmplE.cOm")
+    Event.create!(email: "bar@example.com")
 
-    expect(EventsData.total_events).to eq(1)
+    expect(EventsData.total_events).to eq(2)
 
     result = PersonalDataRemover.call("foo@EXAMPLE.com")
 
     expect(result).to eq "Removed 1 event(s)."
 
-    expect(EventsData.total_events).to eq(0)
+    expect(EventsData.total_events).to eq(1)
     expect { event.reload }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
