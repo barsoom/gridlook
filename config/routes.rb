26321e6dbaae21da385dff5934a11c8bfe9e4dd5
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2019-10-31 19:43:32 +0100

    Clarify where the gridhook route hooks in
    So that you can find the EventProcessor, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Gridlook::Application.routes.draw do
   # Used for checks by deploy scripts
   get "revision" => ->(_) { [ 200, {}, [ ENV.fetch("GIT_COMMIT") ] ] }
 
-  # post "/events" is added by Gridhook
+  # post "/events" is added by Gridhook. See config/initializers/gridhook.rb.
 
   # NOTE: If you change anything here, also check JwtAuthentication config in application.rb
 
