51ac51e1740df963e196b24071826e9daca225d5
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2019-10-31 19:51:20 +0100

    Add user_type and user_id to events
    Not yet used. Let's get it in place before we start using it.
    
    Will be used to identify exactly which user an email was sent to since the email address can change over time.

---
+++ db/migrate/20191031184452_add_user_type_and_user_id_to_events.rb
@@ -0,0 +1,9 @@
+class AddUserTypeAndUserIdToEvents < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
+  def change
+    add_column :events, :user_type, :string, null: true
+    add_column :events, :user_id, :integer, null: true
+    add_index :events, [:user_type, :user_id], algorithm: :concurrently
+  end
+end
