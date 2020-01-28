4f54740229f8a471e1c00a0e5d638556c5e99392
Albert <albert.ramstedt@gmail.com>
2020-01-28 14:27:53 +0100

    Updating migration script since it was very very slow. This method reportedly ran within an hour or so.

--- script/data/set_default_values_and_change_default_table_settings_on_events_associated_records.rb
+++ script/data/set_default_values_and_change_default_table_settings_on_events_associated_records.rb
@@ -1,3 +1,24 @@
-Event.where(associated_records: nil).in_batches.update_all(associated_records: [])
+puts "Starting setting default values #{Time.now}"
+puts
+
+batch_size = 10_000
+
+# This will obviously miss new additions since the migration started, but that should be OK. Setting the column default later should fix them.
+min_id = Event.minimum(:id)
+max_id = Event.maximum(:id)
+
+# Add one to ensure we don't miss anything.
+batch_count = ((max_id - min_id + 1) / batch_size.to_f).ceil + 1
+
+1.upto(batch_count).each do |i|
+  from_id = (i - 1) * batch_size + 1
+  to_id = from_id + batch_size - 1
+  puts "Batch #{i} of #{batch_count}, records #{from_id} – #{to_id}."
+
+  Event.where(id: from_id..to_id).where(associated_records: nil).update_all(associated_records: [])
+end
+
+puts
+puts "Starting default migration... #{Time.now}"
 
 ActiveRecord::Migration.change_column :events, :associated_records, :string, array: true, null: false, default: []
