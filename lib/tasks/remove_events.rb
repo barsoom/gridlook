1ac7e2459d54570980fe8b6cd4a83a2418becdf6
Albert <albert.ramstedt@gmail.com>
2020-09-12 01:19:06 +0200

    drop some events with half the retention setting
    Checked this with the support, which ok:ed it.
    
    SavedSearches are abundant, and most likely not important after a while.
    Campaign mails are most likely never very important but lets keep some
    anyway.

--- lib/tasks/remove_events.rb
+++ lib/tasks/remove_events.rb
@@ -8,16 +8,36 @@ class RemoveEvents
   private
 
   def destroy_events_older_than_the_limit
-    return unless limit
+    return unless event_retention_setting.present?
 
-    # We have no associated relations so we can use delete_all
-    number_of_events_deleted = Event.where("happened_at < ?", limit).delete_all
-    EventsData.instance.decrement!(:total_events, number_of_events_deleted)
-    puts "Deleted events older than #{limit}"
+    remove_events_for_scope(Event.where("happened_at < ?", general_limit))
+    puts "Deleted events older than #{general_limit}"
+
+    # Some events are less important than others
+    remove_events_for_scope(Event.where(mailer_action: "SavedSearchMailer#build").where("happened_at < ?", short_limit))
+    remove_events_for_scope(Event.where("unique_args LIKE '%campaign_id%'").where("happened_at < ?", short_limit))
+    puts "Deleted short-lived-events older than #{short_limit}"
   end
 
-  def limit
-    time = ENV.fetch("NUMBER_OF_MONTHS_TO_KEEP_EVENTS_FOR", nil)
-    time ? time.to_i.months.ago : time
+  def remove_events_for_scope(scope)
+    # We have no associated relations so we can use delete_all
+    number_of_events_deleted = scope.delete_all
+    lower_number_of_event_counter(number_of_events_deleted)
+  end
+
+  def lower_number_of_event_counter(count)
+    EventsData.instance.decrement!(:total_events, count)
+  end
+
+  def general_limit
+    event_retention_setting.to_i.months.ago
+  end
+
+  def short_limit
+    (event_retention_setting.to_i / 2).months.ago
+  end
+
+  def event_retention_setting
+    ENV.fetch("NUMBER_OF_MONTHS_TO_KEEP_EVENTS_FOR", nil)
   end
 end
--- spec/lib/tasks/remove_events_spec.rb
+++ spec/lib/tasks/remove_events_spec.rb
@@ -4,6 +4,11 @@ require "tasks/remove_events"
 describe RemoveEvents, ".call" do
   let(:limit_in_months) { "22" }
 
+  before do
+    # No need for the logging output in the test
+    allow_any_instance_of(RemoveEvents).to receive(:puts)
+  end
+
   around do |test|
     ENV["NUMBER_OF_MONTHS_TO_KEEP_EVENTS_FOR"] = limit_in_months
     test.run
@@ -11,12 +16,40 @@ describe RemoveEvents, ".call" do
   end
 
   it "removes data older than the limit and updates total events" do
-    # No need for the logging output in the test
     limit = limit_in_months.to_i.months.ago
-    allow_any_instance_of(RemoveEvents).to receive(:puts)
 
     event_older_than_the_limit = Event.create!(happened_at: limit - 1.second)
     event_newer_than_the_limit = Event.create!(happened_at: limit + 1.second)
+    expect(Event.count).to eq(2)  # Sanity
+    expect(EventsData.total_events).to eq(2)  # Sanity
+
+    RemoveEvents.call
+
+    expect { event_older_than_the_limit.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    expect { event_newer_than_the_limit.reload }.not_to raise_error
+    expect(EventsData.total_events).to eq(1)
+  end
+
+  it "keeps campaign events half as long as normal events" do
+    limit = (limit_in_months.to_i / 2).months.ago
+
+    event_older_than_the_limit = Event.create!(unique_args: "campaign_id=1", happened_at: limit - 1.second)
+    event_newer_than_the_limit = Event.create!(unique_args: "campaign_id=2", happened_at: limit + 1.second)
+    expect(Event.count).to eq(2)  # Sanity
+    expect(EventsData.total_events).to eq(2)  # Sanity
+
+    RemoveEvents.call
+
+    expect { event_older_than_the_limit.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    expect { event_newer_than_the_limit.reload }.not_to raise_error
+    expect(EventsData.total_events).to eq(1)
+  end
+
+  it "keeps saved_search events half as long as normal events" do
+    limit = (limit_in_months.to_i / 2).months.ago
+
+    event_older_than_the_limit = Event.create!(mailer_action: "SavedSearchMailer#build", happened_at: limit - 1.second)
+    event_newer_than_the_limit = Event.create!(mailer_action: "SavedSearchMailer#build" , happened_at: limit + 1.second)
     expect(Event.count).to eq(2)  # Sanity
     expect(EventsData.total_events).to eq(2)  # Sanity
 
